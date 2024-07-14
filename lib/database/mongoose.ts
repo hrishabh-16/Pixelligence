94311f7a9a2d87191c7a260a586171b06611f3ab
hrishabh-16 <141503421+hrishabh-16@users.noreply.github.com>
2024-07-14 12:55:52 +0530

    db problem solved

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -23,7 +23,7 @@ export const connectToDatabase = async () => {
   cached.promise = 
     cached.promise || 
     mongoose.connect(MONGODB_URL, { 
-      dbName: 'imagilligence', bufferCommands: false 
+      dbName: 'Pixelligence', bufferCommands: false 
     })
 
   cached.conn = await cached.promise;
