1b53c593ab2f887376384f3e4628184b5fe623df
hrishabh-16 <141503421+hrishabh-16@users.noreply.github.com>
2024-07-31 11:39:00 +0530

    updating middleware

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,23 @@
-// import { authMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-// export default authMiddleware({
-//   // Make "/" accessible to all users
-//   publicRoutes: ['/', '/api/webhooks/clerk' , '/api/webhooks/stripe']
-// });
-
-// export const config = {
-//   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-// };
-
-
-import { clerkMiddleware } from '@clerk/nextjs/server';
-
-export default clerkMiddleware()
+export default authMiddleware({
+  // Make "/" accessible to all users
+  publicRoutes: ['/', '/api/webhooks/clerk' , '/api/webhooks/stripe']
+});
 
 export const config = {
-
-  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+
+// import { clerkMiddleware } from '@clerk/nextjs/server';
+
+// export default clerkMiddleware()
+
+// export const config = {
+
+//   matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
 
 
 // import { clerkMiddleware } from "@clerk/nextjs/server";
